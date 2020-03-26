135c2f0e9fe955bd6e030adf22a0cef3f4950185
Eduardo Kasper <Eduardo_Kasper@gap.com>
2020-03-26 11:27:08 -0300

    creates function to determine what is a weekday

--- src/hotel_reservation/core.clj
+++ src/hotel_reservation/core.clj
@@ -1,14 +1,29 @@
 (ns hotel-reservation.core)
 
 (def lakewood
-  { :classification 3 :weekdays 110 :weekdaysReward 80 :weekends 90 :weekendsReward 80 })
+  { :classification 3
+    :weekdays       110
+    :weekdaysReward 80
+    :weekends       90
+    :weekendsReward 80 })
 
 (def bridgewood
-  { :classification 4 :weekdays 160 :weekdaysReward 110 :weekends 60 :weekendsReward 50 })
+  { :classification 4
+    :weekdays       160
+    :weekdaysReward 110
+    :weekends       60
+    :weekendsReward 50 })
 
 (def ridgewood
-  { :classification 5 :weekdays 220 :weekdaysReward 100 :weekends 150 :weekendsReward 40 })
+  { :classification 5
+    :weekdays       220
+    :weekdaysReward 100
+    :weekends       150
+    :weekendsReward 40 })
 
+
+(defn is-weekday [day]
+  (and (not= day "sat")) (not= day "sun"))
 
 (def str-to-date-formater
   (java.text.SimpleDateFormat. "ddEyyyy"))
--- test/hotel_reservation/core_test.clj
+++ test/hotel_reservation/core_test.clj
@@ -3,11 +3,34 @@
             [hotel-reservation.core :refer :all]))
 
 (deftest hotel-structures
-  (testing "Lakewood"
-           (lakewood { :classification 3 :weekdays 110 :weekdaysReward 80 :weekends 90 :weekendsReward 80 })
-           (bridgewood { :classification 4 :weekdays 160 :weekdaysReward 110 :weekends 60 :weekendsReward 50 })
-           (ridgewood { :classification 5 :weekdays 220 :weekdaysReward 100 :weekends 150 :weekendsReward 40 })
+  (testing "hotel objects"
+           (lakewood { :classification  3
+                       :weekdays        110
+                       :weekdaysReward  80
+                       :weekends        90
+                       :weekendsReward  80 })
+           (bridgewood { :classification  4
+                         :weekdays        160
+                         :weekdaysReward  110
+                         :weekends        60
+                         :weekendsReward  50 })
+           (ridgewood { :classification   5
+                        :weekdays         220
+                        :weekdaysReward   100
+                        :weekends         150
+                        :weekendsReward   40 })
    ))
+
+(deftest parsing-input
+  (testing "should return true when input is a weekday"
+    (is (is-weekday "mon"))
+    (is (is-weekday "tues"))
+    (is (is-weekday "wed"))
+    (is (is-weekday "thu"))
+    (is (is-weekday "fri")))
+  (testing "should return false when input is a weekend"
+    (not (is-weekday "sat")))
+    (not (is-weekday "sun")))
 
 (deftest test-date-format
   (testing "simple format"
